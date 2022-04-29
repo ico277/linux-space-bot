b4b6e5d7cb3e3654b5489dc6586a9a645a9b11aa
ico277 <enfrico123@gmail.com>
2022-04-30 00:47:26 +0200

    v.1.1.0 - added a few commands - added a license that I forgot to add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const Config = require('../config/config.json');
+const { Routes: routes } = require('discord-api-types/v9');
+const config = require('../config/config.json');
 const wait = require('util').promisify(setTimeout);
+const fs = require('fs');
+
+const warns_file = __dirname + '/../warns/warns.json';
+let warns = {};
+console.log(`Loading ${warns_file}...`);
+if (fs.existsSync(warns_file)) {
+    console.log(`Reading ${warns_file}...`);
+    let bytes = fs.readFileSync(warns_file, 'ascii');
+    try {
+        warns = JSON.parse(bytes);
+    } catch (err) {
+        console.error(`Error reading ${warns_file}!`);
+        console.error(err);
+        process.exit(1);
+    }
+} else {
+    console.log(`File ${warns_file} does not exist!`)
+    console.log(`Creating ${warns_file}...`);
+    try {
+        fs.writeFileSync(warns_file, '{}\n');
+    } catch (err) {
+        console.error(`Error writing ${warns_file}!`);
+        console.error(err);
+        process.exit(1);
+    }
+}
+console.log(`Successfully loaded ${warns_file}!`);
 
 const { global_commands: GlobalCommands } = require('../config/commands.json');
 const { linux_space_commands: LinuxSpaceCommands } = require('../config/commands.json');
@@ -13,11 +40,11 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
         console.log('Started refreshing application (/) commands.');
 
         await rest.put(
-            Routes.applicationCommands('905545640644337674'),
+            routes.applicationCommands('905545640644337674'),
             { body: GlobalCommands },
         );
         await rest.put(
-            Routes.applicationGuildCommands('905545640644337674', '886284556624347176'),
+            routes.applicationGuildCommands('905545640644337674', '886284556624347176'),
             { body: LinuxSpaceCommands }
         );
 
@@ -44,7 +71,7 @@ async function log_channel(...attachments) {
     channel = channel ? channel : client.channels.fetch(channel_id);
     if (channel) {
         try {
-            return await channel.send({embeds: attachments});
+            return await channel.send({ embeds: attachments });
         } catch (err) {
             throw { error: err, msg: 'Error sending message. Check bot permission' };
         }
@@ -53,16 +80,19 @@ async function log_channel(...attachments) {
     }
 }
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
+    // Misc commands
     if (interaction.commandName === 'ping') {
         await interaction.reply('Pong!');
-    } else if (interaction.commandName === 'ban') {
+    }
+    // Moderation commands
+    else if (interaction.commandName === 'ban') {
         if (!interaction.member.permissions.has('BAN_MEMBERS')) {
             await interaction.reply('You need ban members permission to use this command!');
             return;
@@ -94,64 +124,43 @@ client.on('interactionCreate', async interaction => {
                 false
             )
             .setTimestamp(Date.now())
-            .setColor(Config.colors.embed);
-        log_channel(embed)
-            .then(async msg => {
-                await msg.pin();
-            })
-            .catch(async err => {
-                await interaction.editReply(`Error whilst writing to log channel:\n${err.msg}`)
-                if (err.error) console.error(err.error);
-            });
+            .setColor(config.colors.embed);
+        await log_channel(embed);
     } else if (interaction.commandName === 'kick') {
         if (!interaction.member.permissions.has('KICK_MEMBERS')) {
             await interaction.reply('You need kick members permission to use this command!');
             return;
         }
 
-        let member = interaction.options.getMember('member');
-        let member_user = member.user;
-        let reason = interaction.options.getString('reason');
+        let member = interaction.options.getMember('member', true);
+        let reason = interaction.options.getString('reason', false);
         reason = reason ? reason : '<No reason provided>';
-        if (!member) {
-            await interaction.reply('You need to specify a member!');
-            return;
-        }
 
         if (!member.kickable) {
             await interaction.reply(`Cannot kick ${member}. Check bot permission`);
             return;
         }
-        await member.kick({ reason: reason });
+        await member.kick(reason);
         await interaction.reply(`Successfully kicked ${member}.`);
-
-        console.log("reason %s", reason)
 
         let embed = new MessageEmbed()
             .setTitle('Member Kicked')
-            .addField('Member', `${member_user.tag} (${member_user.id})`, false)
+            .addField('Member', `${member.user.tag} (${member.user.id})`, false)
             .addField('Reason', reason, false)
             .addField('Moderator',
-                `${interaction.user.tag} ${interaction.member.nickname ? `(aka ${interaction.member.nickname})` : ''} (${interaction.user.id})`,
+                `${interaction.user.tag} ${interaction.member.nickname ? `(${interaction.member.nickname})` : ''} (${interaction.user.id})`,
                 false
             )
             .setTimestamp(Date.now())
-            .setColor(Config.colors.embed);
-        log_channel(embed)
-            .then(async msg => {
-                await msg.pin();
-            })
-            .catch(async err => {
-                await interaction.editReply(`Error whilst writing to log channel:\n${err.msg}`)
-                if (err.error) console.error(err.error);
-            });
+            .setColor(config.colors.embed_critical);
+        await log_channel(embed);
     } else if (interaction.commandName === 'clear') {
-        if (!interaction.member.permissions.has('BAN_MEMBERS')) {
+        if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
             await interaction.reply('You need manage messages permission to use this command!');
             return;
         }
 
-        let amount = interaction.options.getInteger('messages');
+        let amount = interaction.options.getInteger('messages', true);
         let channel = interaction.channel;
         try {
             await channel.bulkDelete(amount);
@@ -160,6 +169,84 @@ client.on('interactionCreate', async interaction => {
             await interaction.deleteReply();
         } catch (err) {
             await interaction.reply(`There was an error deleting ${amount} messages. Check permission`);
+            console.error(err);
+        }
+    } else if (interaction.commandName === 'warn') {
+        if (!interaction.member.permissions.has('KICK_MEMBERS') || !interaction.member.permissions.has('BAN_MEMBERS')) {
+            await interaction.reply('You need kick and ban members permission to use this command!');
+            return;
+        }
+
+        let member = interaction.options.getMember('member', true);
+        let reason = interaction.options.getString('reason', false);
+
+        if (!warns.hasOwnProperty(interaction.guildId)) {
+            warns[interaction.guildId] = {};
+            warns[interaction.guildId][member.id] = { "warns": 1 }
+        } else if (!warns[interaction.guildId].hasOwnProperty(member.id)) {
+            warns[interaction.guildId][member.id] = { "warns": 1 }
+        } else {
+            warns[interaction.guildId][member.id].warns++;
+        }
+
+        reason = reason ? reason : '<No reason provided>';
+        let embed = new MessageEmbed()
+            .setTitle('Member Warn')
+            .addField('Member', `${member.user.tag} ${member.nick ? `(${member.nickname})` : ''} (${member.id})`, false)
+            .addField('Moderator',
+                `${interaction.user.tag} ${interaction.member.nickname ? `(${interaction.member.nickname})` : ''} (${interaction.user.id})`,
+                false
+            )
+            .addField('Reason', reason, true)
+            .setTimestamp(Date.now())
+            .setColor(config.colors.embed)
+            .setThumbnail(member.user.avatarURL() ? member.user.avatarURL() : client.user.avatarURL());
+
+        let warn_pretty_num;
+        if (warns[interaction.guildId][member.id].warns === 1)
+            warn_pretty_num = '1st';
+        else if (warns[interaction.guildId][member.id].warns === 2)
+            warn_pretty_num = '2nd';
+        else if (warns[interaction.guildId][member.id].warns === 3)
+            warn_pretty_num = '3rd';
+        else
+            warn_pretty_num = warns[interaction.guildId][member.id].warns + 'th';
+        embed.setFooter({ text: `This is their ${warn_pretty_num} warn!` });
+
+        if (warns[interaction.guildId][member.id].warns < 3) {
+            embed.addField('Punishment', 'None', true);
+            await log_channel(embed);
+            await interaction.reply(`Successfully warned ${member.user.tag}!`);
+        } else if (warns[interaction.guildId][member.id].warns == 3) {
+            embed.setColor(config.colors.embed_critical);
+            if (!member.kickable) {
+                await interaction.reply('Error: cannot kick member!\nWarned the user anyways.');
+                embed.addField('Punishment', 'Failed Kick', true);
+            } else {
+                await member.kick(reason);
+                await interaction.reply(`Successfully kicked ${member.user.tag} due to 3rd warn!`);
+                embed.addField('Punishment', 'Kick', true);
+            }
+            await log_channel(embed);
+        } else {
+            embed.setColor(config.colors.embed_critical);
+            if (!member.bannable) {
+                await interaction.reply('Error: cannot ban member!\nWarned the user anyways.');
+                embed.addField('Punishment', 'Failed Ban', true)
+            } else {
+                await member.ban({ reason: reason });
+                await interaction.reply(`Successfully banned ${member.user.tag} due to 4 or more warnings!`);
+                embed.addField('Punishment', 'Ban', true);
+            }
+            await log_channel(embed);
+        }
+
+        try {
+            console.log(`Saving ${warns_file}...`);
+            fs.writeFileSync(warns_file, JSON.stringify(warns, undefined, 4), { encoding: 'ascii' });
+            console.log(`Successfully saved ${warns_file}!`);
+        } catch (err) {
+            console.error(`Could not write to ${warns_file}!`);
             console.error(err);
         }
     }
